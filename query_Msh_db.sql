531d56c8f9beb13a5a82155b2a49a145de6d6290
Kean Duque <vizen27@users.noreply.github.com>
2022-12-20 10:57:14 +0100

    db change
    changes

--- query_Msh_db.sql
+++ query_Msh_db.sql
@@ -326,12 +326,53 @@ FROM products p
 CROSS JOIN shippers s
 ORDER BY s.name ASC;
 
--- UNION 
-SELECT * 
-FROM orders;
+-- UNION to combined records from multiple queries to rows
+SELECT 
+	order_id,
+    order_date,
+    'Active' AS status
+FROM orders
+WHERE order_date >= '2019-01-01'
+UNION
+SELECT 
+	order_id,
+    order_date,
+    'Archived' AS status
+FROM orders
+WHERE order_date < '2019-01-01';
 
+SELECT first_name
+FROM customers
+UNION
+SELECT name
+FROM shippers;
 
+SELECT
+	customer_id,
+    first_name,
+    points,
+    'Bronze' AS type
+FROM customers
+WHERE points < 2000
+UNION
+SELECT
+	customer_id,
+    first_name,
+    points,
+    'Silver' AS type
+FROM customers
+WHERE points BETWEEN 2000 AND 3000
+UNION
+SELECT
+	customer_id,
+    first_name,
+    points,
+    'Gold' AS type
+FROM customers
+WHERE points > 3000
+ORDER BY first_name ASC;
 
+-- COLUMN Attributes 
 
 
 
